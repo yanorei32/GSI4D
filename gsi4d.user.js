8e5ce4503b324bac487689d93552ccb029a60c65
Yanorei32 <yanorei@hotmail.co.jp>
2018-11-08 14:41:10 +0900

    Add docker hub

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version	 	0.1.1
+// @version	 	0.1.2
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -28,6 +28,9 @@
 
 		// RedHat
 		'access.redhat.com/documentation',
+
+		// Docker
+		'hub.docker.com',
 
 		// GitHub
 		'github.com',
