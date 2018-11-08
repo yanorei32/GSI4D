96625643d338ce07d5c67b14a509bd17d9b12317
Yanorei32 <yanorei@hotmail.co.jp>
2018-11-08 14:55:12 +0900

    Fix typo

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version	 	0.1.3
+// @version	 	0.1.4
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -43,7 +43,7 @@
 		'railsdoc.com',
 
 		// Ruby - gem
-		'rubygems.org'
+		'rubygems.org',
 
 		// Python - requests
 		'requests-docs-ja.readthedocs.io',
