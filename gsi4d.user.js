e0984d88ae9750a6e3396b44a3b030a33bd0bb2d
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2018-11-09 17:16:47 +0900

    Add askubuntu.com

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version	 	0.1.4
+// @version	 	0.1.5
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -67,6 +67,7 @@
 		'stackoverflow.com',
 		'superuser.com',
 		'teratail.com',
+		'askubuntu.com',
 	];
 
 	// 任意のユーザーが使えるサービス。比較的良質な物が多い。
