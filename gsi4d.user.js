177da06dea7ce1f0712a9f244b29f515cb7c288e
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2018-11-22 16:53:42 +0900

    Fix indent (tab 4)

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version	 	0.2.0
+// @version		0.2.0
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -35,10 +35,10 @@
 
 		// GitHub
 		'github.com',
-		
+
 		// Golang
 		'tour.golang.org',
-		
+
 		// Ruby
 		'ruby-lang.org',
 		'ruby-doc.org',
