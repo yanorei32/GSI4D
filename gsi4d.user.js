e63d4fc8a97167436748f44c9f6b941f66cb202e
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2018-11-22 15:13:18 +0900

    Fix version

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version	 	0.1.7
+// @version	 	0.1.8
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
