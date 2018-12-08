b758f08a1959f204c613f11ffb6cd8208364ef10
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2018-12-08 20:26:04 +0900

    Add solvusoft.com to BLACK_LIST.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.2.5
+// @version		0.2.6
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -96,6 +96,9 @@
 		// StackExchange等からコピーしGoogle翻訳にかけたサイト。
 		'stackovernet.com',
 		'stackoverrun.com',
+		
+		// AdSite
+		'solvusoft.com',
 	];
 
 	const changeColor = (link, log) => {
