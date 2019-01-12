6f8d6ec2ac41fbd84d0cf1a1fe1d3281dfc49dde
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-01-13 02:30:37 +0900

    Add programming-study.com to BLACK_LIST.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.4.0
+// @version		0.4.1
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -92,6 +92,7 @@
 
 		// 画像が多くて嫌い。
 		'techacademy.jp',
+		'programming-study.com',
 
 		// Google翻訳されたかのような謎文章のサイト。
 		'code.i-harness.com',
