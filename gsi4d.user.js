ac58293066409081cbf7febc8ed62df0b1df0d65
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-01-20 00:36:56 +0900

    Add profession-net.com to BLACK_LIST

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.4.4
+// @version		0.4.5
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -85,6 +85,9 @@
 	];
 
 	const BLACK_LIST = [
+		// 誤った情報を大々的に掲載している。
+		'profession-net.com',
+		
 		// ADBlockerが有効だとコンテンツを見せない
 		'server-setting.info',
 
