b76f3836f7dfae1e63b86c0708e017246a022e30
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-01-22 15:26:11 +0900

    Add windows.com to REFERENCE_LIST.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.4.8
+// @version		0.4.9
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -16,6 +16,9 @@
 	'use strict';
 
 	const REFERENCE_LIST = [
+		// Microsoft
+		'windows.com',
+		
 		// Mozilla
 		'developer.mozilla.org',
 
