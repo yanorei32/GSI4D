425d9864807c2f1eb6e6456a1daf883b6c70e533
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-04-21 22:41:47 +0900

    Add employment.en-japan.com to BLACK_LIST.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.4.10
+// @version		0.4.11
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -94,6 +94,9 @@
 	];
 
 	const BLACK_LIST = [
+		// 微妙な入門講座が引っかかった事があった。
+		'employment.en-japan.com',
+
 		// 誤った情報を大々的に掲載している。
 		'profession-net.com',
 		
