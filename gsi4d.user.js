ee0db15a75864ed637f1247383489092f21840e2
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-05-19 01:15:39 +0900

    Add eng-entrance.com to BLACK_LIST.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.4.17
+// @version		0.4.18
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -119,6 +119,7 @@
 		'udemy.benesse.co.jp',
 		'techplay.jp',
 		'furien.jp',
+		'eng-entrance.com',
 
 		// Google翻訳されたかのような謎文章のサイト。
 		'code.i-harness.com',
