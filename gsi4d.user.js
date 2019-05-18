788adbfca78396e9d6d42252c3cecc190d480a88
yuuta1100883 <49468062+yuuta1100883@users.noreply.github.com>
2019-05-19 08:04:41 +0900

    Add jsprimer.net to RECOM_LIST.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.4.18
+// @version		0.4.19
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -69,6 +69,9 @@
 		// Web系を調べていると行き着く。
 		'tohoho-web.com',
 		'htmq.com',
+
+		// JavaScript周りを調べると行き着く。
+		'jsprimer.net',
 
 		// VBやC#を調べていると行き着く。
 		'dobon.net',
