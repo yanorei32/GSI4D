f31813b2c77b3a614ba5f17006c1fb8f63f77a98
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-07-16 14:51:34 +0900

    Add php.net to REFERENCE_LIST.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.4.21
+// @version		0.4.22
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -63,6 +63,9 @@
 		
 		// w3school.com
 		'w3schools.com',
+		
+		// PHP
+		'php.net',
 	];
 
 	const RECOM_LIST = [
