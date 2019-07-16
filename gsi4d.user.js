a5af30f832098e075eac3e190dadbca7ecb4ec50
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-07-16 14:50:18 +0900

    Add phpspot.net/php/man/ to BLACK_LIST.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.4.20
+// @version		0.4.21
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -102,6 +102,9 @@
 	];
 
 	const BLACK_LIST = [
+		// 主なコンテンツはまともだが、manはphp.netと同一
+		'phpspot.net/php/man/',
+		
 		// 微妙な入門講座が引っかかった事があった。
 		'employment.en-japan.com',
 
