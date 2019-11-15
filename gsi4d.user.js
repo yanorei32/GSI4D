9973a5ee1e05e6491951698a909b816867763a05
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-11-15 21:54:54 +0900

    Add ufcpp.net to RECOM_LIST.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.4.29
+// @version		0.4.30
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -103,6 +103,7 @@
 		// C++を調べていたときに行き着いた。
 		'cpprefjp.github.io',
 		'cppreference.com',
+		'ufcpp.net',
 	];
 
 	const RECOM_FORUM = [
