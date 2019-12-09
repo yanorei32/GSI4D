a31bea5cd631e7865ef9911579d28e76e924bddb
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-12-09 11:12:20 +0900

    Add answers.unity.com to recommendForum.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.8.7
+// @version		0.8.8
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -130,6 +130,7 @@
 				'teratail.com',
 				'askubuntu.com',
 				'reddit.com',
+				'answers.unity.com',
 			],
 		},
 
