56bd6134066319473aac1ab17c3cc2db2f46214d
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2019-12-10 23:31:21 +0900

    Add some too low quality site to blackList.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.8.9
+// @version		0.8.10
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -156,8 +156,10 @@
 				// 誤った情報を大々的に掲載している。
 				'profession-net.com',
 				'symmetric.co.jp',
+				'mupon.net',
+				'marucoblog.com',
+				'suke-log.com',
 
-				
 				// ADBlockerが有効だとコンテンツを見せない
 				'server-setting.info',
 
