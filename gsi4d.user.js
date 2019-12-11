95c19de1c636ad56507861e23f660c72c0c51dc5
Yanorei32 <yanorei@hotmail.co.jp>
2019-12-11 15:15:45 +0900

    Update engine

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		0.8.10
+// @version		1.0.0
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -25,10 +25,10 @@
 				'windows.com',
 				'msdn.microsoft.com',
 				'docs.microsoft.com',
-				
+
 				// Oracle
 				'docs.oracle.com',
-				
+
 				// Mozilla
 				'developer.mozilla.org',
 
@@ -44,10 +44,10 @@
 
 				// GitHub
 				'github.com',
-				
+
 				// OSDN
 				'osdn.net',
-				
+
 				// sourceforge
 				'sourceforge.net',
 
@@ -72,13 +72,13 @@
 
 				// IPA
 				'ipa.go.jp',
-				
+
 				// w3school.com
 				'w3schools.com',
-				
+
 				// PHP
 				'php.net',
-				
+
 				// Apache
 				'apache.org',
 
@@ -103,23 +103,23 @@
 
 				// Python周りを調べると行き着く。
 				'note.nkmk.me',
-				
+
 				// Win2Kを調べると行き着く。
 				'blog.livedoor.jp/blackwingcat',
-				
+
 				// Go周りを調べると行き着く。
 				'ashitani.jp/golangtips/',
-				
+
 				// Apacheを調べていて行き着いた。
 				'park12.wakwak.com/~eslab/pcmemo/',
-				
+
 				// C++を調べていたときに行き着いた。
 				'cpprefjp.github.io',
 				'cppreference.com',
 				'ufcpp.net',
 			],
 		},
-		
+
 		recommendForum: {
 			isBlacklist: false,
 			color: '#EEE',
@@ -145,7 +145,7 @@
 
 		blackList: {
 			isBlacklist: true,
-			color: undefined,
+			color: null,
 			list: [
 				// Wikipedia スクレイピング(?)
 				'wikiwand.com',
@@ -210,8 +210,20 @@
 		},
 	};
 
+	const SearchTypes = {
+		Default:	'',
+		Image:		'isch',
+		Video:		'vid',
+		News:		'nws',
+		Shop:		'shop',
+		Books:		'bks',
+		Patent:		'ptd',
+		Unknown:	null,
+	};
+
 	const deleteElement = (e) => {
-		e.parentElement.removeChild(e);
+		if (e.parentElement !== null)
+			e.parentElement.removeChild(e);
 	};
 
 	const formatGSI4DLog = (log) => {
@@ -220,40 +232,106 @@
 
 	const pageStyle = {
 		PC: {
-			getLinkElems: () => {
-				return document.querySelectorAll('div.r>a');
+			supportSearchTypes: [
+				SearchTypes.Default,
+				SearchTypes.Video,
+				SearchTypes.Image,
+			],
+
+			getLinkElems: (st, parentE) => {
+				if (st === SearchTypes.Image) 
+					return parentE.querySelectorAll('div#rg_s>div>div.rg_meta.notranslate');
+
+				return parentE.querySelectorAll('div.r>a:not(.fl)');
 			},
 
-			coloriseCandidateByLinkElem: (linkE, color) => {
-				return linkE.parentElement.parentElement.parentElement.parentElement.style.backgroundColor = color;
+			coloriseCandidateByLinkElem: (st, linkE, color) => {
+				if (st === SearchTypes.Image) {
+					linkE.parentElement.style.backgroundColor = color;
+					return;
+				}
+
+				linkE.parentElement.parentElement.parentElement.parentElement.style.backgroundColor = color;
 			},
 
-			deleteCandidateByLinkElem: (linkE) => {
+			deleteCandidateByLinkElem: (st, linkE) => {
+				if (st === SearchTypes.Image) {
+					deleteElement(linkE.parentElement);
+					return;
+				}
+
 				deleteElement(
 					linkE.parentElement.parentElement.parentElement.parentElement
 				);
 			},
 
-			writeLog: (formatedLog) => {
-				document.getElementById('resultStats').innerHTML += formatedLog;
+			writeLog: (st, formatedLog) => {
+				if (st !== SearchTypes.Image) {
+					document.getElementById('resultStats').innerHTML += formatedLog;
+					return;
+				}
+
+				const div = document.createElement('div');
+				div.textContent = 'GSI4D: Observing...';
+				document.getElementById('topstuff').appendChild(div);
+			},
+
+			after: (st) => {
+				if (st !== SearchTypes.Image)
+					return;
+
+				(new MutationObserver(( records ) => {
+					for (const record of records)
+						for (const addedNode of record.addedNodes)
+							if (addedNode.nodeType == Node.ELEMENT_NODE)
+								linkProcess(st, addedNode);
+
+				})).observe(
+					document.getElementById('rg_s'),
+					{ childList: true },
+				);
 			},
 		},
 		Phone: {
-			getLinkElems: () => {
-				return document.querySelectorAll('div.kCrYT>a');
+			supportSearchTypes: [
+				SearchTypes.Default,
+				SearchTypes.Video,
+				SearchTypes.Image,
+			],
+
+			getLinkElems: (st, parentE) => {
+				if (st === SearchTypes.Image)
+					return document.querySelectorAll('.iKjWAf');
+
+				return parentE.querySelectorAll('div.kCrYT>a');
 			},
 
-			coloriseCandidateByLinkElem: (linkE, color) => {
+			coloriseCandidateByLinkElem: (st, linkE, color) => {
+				if (st === SearchTypes.Image) {
+					linkE.style.backgroundColor = color;
+					return;
+				}
+
 				linkE.parentElement.parentElement.style.backgroundColor = color;
 			},
 
-			deleteCandidateByLinkElem: (linkE) => {
+			deleteCandidateByLinkElem: (st, linkE) => {
+				if (st === SearchTypes.Image) {
+					deleteElement(
+						linkE.parentElement
+					);
+					return;
+				}
+
 				deleteElement(
 					linkE.parentElement.parentElement.parentElement
 				);
 			},
 
-			writeLog: (formatedLog) => {
+			writeLog: (st, formatedLog) => {
+				if (st === SearchTypes.Image)
+					return;
+
 				const cardContainer = document.createElement('div');
 				const card = document.createElement('div');
 				const cardSpan = document.createElement('span');
@@ -269,11 +347,27 @@
 				cardContainer.appendChild(card);
 				document.getElementById('main').childNodes[1].after(cardContainer);
 			},
+
+			after: (st) => {
+				if (st !== SearchTypes.Image)
+					return;
+
+				for (const col of document.querySelectorAll('div#islrg>div>div'))
+					(new MutationObserver(( records ) => {
+						for (const record of records)
+							for (const addedNode of record.addedNodes)
+								if (addedNode.nodeType == Node.ELEMENT_NODE)
+									linkProcess(st, addedNode);
+					})).observe(
+						col,
+						{ childList: true },
+					);
+			},
 		},
 	};
 
 	const getPageStyle = () => {
-		if (document.getElementById('resultStats') === null) {
+		if (document.getElementById('searchform') === null) {
 			console.log('PageStyle: Phone');
 			return pageStyle.Phone;
 		}
@@ -283,7 +377,7 @@
 	};
 
 	const parseURL = (url) => {
-		if (url.indexOf('://'))
+		if (!url.startsWith('/url?') && url.indexOf('://'))
 			return url;
 
 		if (!url.startsWith('/url?')) {
@@ -294,13 +388,17 @@
 		const paramStrs = url.split('?')[1].split('&');
 
 		for (const paramStr of paramStrs) {
-			const parsedParam = parsedParam.split('=');
-			
-			if (parsedParam[0] != 'q')
+			const parsedParam = paramStr.split('=');
+
+			if (parsedParam[0] !== 'q')
 				continue;
 
-			if (parsedParam.length != 2)
+			if (parsedParam.length !== 2)
 				continue;
+
+			if (parsedParam[1].indexOf('://')) {
+				return parsedParam[1];
+			}
 
 			return decodeURI(parsedParam[1]); 
 		}
@@ -309,47 +407,103 @@
 		return '';
 	};
 
+	const parseSearchURL = (url) => {
+		const paramStrs = url.split('?')[1].split('&');
 
-	const p = getPageStyle();
-	const log = {
-		blockedCount: 0,
-		trackedCount: 0,
+		for (const paramStr of paramStrs) {
+			const parsedParam = paramStr.split('=');
+
+			if (parsedParam[0] !== 'tbm')
+				continue;
+
+			if (parsedParam.length !== 2)
+				continue;
+
+			for (const [_, searchType] of Object.entries(SearchTypes))
+				if (parsedParam[1] === searchType)
+					return searchType;
+
+			return SearchTypes.Unknown;
+		}
+
+		return SearchTypes.Default;
 	};
 
-	linkLoop:
-	for (const link of p.getLinkElems()) {
+	const parseLinkElement = (st, link) => {
+		if (link.tagName === 'DIV') {
+			const parsedJSON = JSON.parse(link.innerHTML);
+
+			if (parsedJSON['ru'] == undefined) {
+				console.error('Link element has not ru.');
+				console.error(link);
+				return '';
+			}
+
+			return parsedJSON['ru'];
+		}
+
 		const rawHref = link.getAttribute('href');
 
 		if (rawHref === null) {
 			console.error('Link element has not href.');
 			console.error(link);
-			continue linkLoop;
+			return '';
 		}
 
-		const url = parseURL(rawHref);
-		if (url === '')
-			continue linkLoop;
+		return parseURL(rawHref);
+	};
 
-		for (const [_, siteType] of Object.entries(site)) {
-			for (const siteStr of siteType.list) {
-				if (rawHref.indexOf(siteStr) === -1)
-					continue;
+	const linkProcess = (st, parentE) => {
+		linkLoop:
+		for (const link of p.getLinkElems(st, parentE)) {
+			const url = parseLinkElement(st, link);
+			if (url === '')
+				continue linkLoop;
+			
+			for (const [_, siteType] of Object.entries(site)) {
+				for (const siteStr of siteType.list) {
+					if (url.indexOf(siteStr) === -1)
+						continue;
 
-				/* found! yay! */
+					/* found! yay! */
 
-				if (siteType.isBlacklist) {
-					p.deleteCandidateByLinkElem(link);
-					log.blockedCount ++;
+					if (siteType.isBlacklist) {
+						p.deleteCandidateByLinkElem(st, link);
+						log.blockedCount ++;
+						continue linkLoop;
+					}
+
+					p.coloriseCandidateByLinkElem(st, link, siteType.color);
+					log.trackedCount ++;
 					continue linkLoop;
 				}
-
-				p.coloriseCandidateByLinkElem(link, siteType.color);
-				log.trackedCount ++;
-				continue linkLoop;
 			}
 		}
+	};
+
+	const currentST = parseSearchURL(location.href);
+
+	if (currentST === SearchTypes.Unknown) {
+		console.debug('Unknown search type');
+		return;
 	}
 
-	p.writeLog(formatGSI4DLog(log));
+	const p = getPageStyle();
+
+	if (!p.supportSearchTypes.includes(currentST)) {
+		console.debug('Unsupported search type in this platform.');
+		return;
+	}
+
+	const log = {
+		blockedCount: 0,
+		trackedCount: 0,
+	};
+
+	linkProcess(currentST, document);
+
+	p.writeLog(currentST, formatGSI4DLog(log));
+
+	p.after(currentST);
 })();
 
