3ba076f1d8edcce5363ead2fff7d3c281bbf26f5
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2020-11-27 04:24:29 +0900

    Add sobrelinux.info to BLACK_LIST

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		1.2.8
+// @version		1.2.9
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/Yanorei32/GSI4D/issues
@@ -199,6 +199,7 @@
 				'qastack.jp',
 				'coder.work',
 				'it-swarm.dev',
+				'sobrelinux.info',
 
 				// AdSite
 				'solvusoft.com',
