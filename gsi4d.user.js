2b04ad286af091e0ac1f0d517ceaa1381379dee2
Ray Yano | あねてあ <yanorei@hotmail.co.jp>
2023-09-17 11:44:36 +0900

    Bump up engine version to 4.

--- gsi4d.user.js
+++ gsi4d.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name		GSI4D - Google search improve for developers
 // @description	Google search improve for developers.
-// @version		1.3.10
+// @version		1.4.10
 // @include		/^https://www\.google\.co(m|\.jp)/search.+$/
 // @author		yanorei32
 // @supportURL	https://github.com/yanorei32/GSI4D/issues
